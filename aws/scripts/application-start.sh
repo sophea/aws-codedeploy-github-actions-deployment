80219223a13f2a7a1cc51e384582bfefa06dc7fe
Sophea Mak <sophea.mak@jtrustroyal.com>
2022-04-13 17:24:20 +0700

    update aws application-start.sh

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -xe
 
-
 APP_NAME='demo'
 HOME_FOLDER='/opt/webapp'
-JAR_FILE=`ls -t $HOME_FOLDER/*.jar | head -1`
-sudo mv $JAR_FILE $HOME_FOLDER/${APP_NAME}.jar
 sudo ln -s $HOME_FOLDER/${APP_NAME}.jar /etc/init.d/${APP_NAME}
 sudo service ${APP_NAME} start
