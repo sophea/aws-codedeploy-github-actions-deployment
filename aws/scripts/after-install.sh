4ba5f275fdcb9e6d7b66f154a75e8531739b5283
Sophea Mak <sophea.mak@jtrustroyal.com>
2022-04-13 16:53:22 +0700

    update s3 bucket

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,5 +3,5 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://aws-codeploy-github-action-webappdeploymentbucket-1f4vuw3u5wdzn/demo.jar /opt/webapp/demo.jar
+aws s3 cp s3://aws-code-deploy-github-webappdeploymentbucket-14sttyakht7mr/demo.jar /opt/webapp/demo.jar
 
