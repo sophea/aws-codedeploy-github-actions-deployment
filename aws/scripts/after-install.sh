3701ef5546caa16dac771dd9b03f2e161b1f7754
Sophea Mak <sophea.mak@jtrustroyal.com>
2022-04-13 15:16:12 +0700

    update s3 bucket and region

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://##s3-bucket##/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://aws-codeploy-github-action-webappdeploymentbucket-1f4vuw3u5wdzn/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
